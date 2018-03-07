d9b2492448319ef036356cddfbfba867d129af6a
Louis Medina <louismedina@Louiss-MacBook-Pro.local>
2018-03-07 17:53:30 -0500

    Initial Commit

---
+++ solution-code/contact-app/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactFilterPipe } from './pipes/contact-filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactListComponent,
+    ContactFilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ solution-code/contact-app/src/app/contact-list/contact-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import contactList from '../contacts';
+
+@Component({
+  selector: 'app-contact-list',
+  templateUrl: './contact-list.component.html',
+  styleUrls: ['./contact-list.component.css']
+})
+export class ContactListComponent implements OnInit {
+  contacts: Object[];
+  newContact: Object = {};
+  selectedField: string = "name";
+  pattern: string = "";
+  
+  constructor() { }
+
+  ngOnInit() {
+    this.contacts = contactList;
+  }
+
+  addContact(){
+    this.contacts.unshift(this.newContact);
+    this.newContact = {};
+  }
+}
